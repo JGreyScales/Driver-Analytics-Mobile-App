1e633e1bc79685aa004b01646938c79d9a1d1535
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-29 18:04:26 -0400

    test cases made

--- back-end-api/middleware/auth.js
+++ back-end-api/middleware/auth.js
@@ -1,17 +1,27 @@
 const jwt = require("jsonwebtoken")
 const dataType = require('../utils/dataType')
+require("dotenv").config({quiet: true});
 
 class JWT_AUTH {
     constructor(){}
 
     static __extractToken(req){
         let token = undefined;
-        if (typeof(req) === 'string'){token = req?.split(' ')[1]}
-        else {token = req.header('Authorization')?.split(' ')[1];}
-        return token
+        try{
+            if (typeof(req) === 'string'){token = req?.split(' ')[1]}
+            else {token = req.header('Authorization')?.split(' ')[1];}
+        } catch {
+            token = ""
+        } finally {
+            return token
+        }
     }
 
     static generateToken(userID){
+        // empty string on failure
+
+        if (!dataType.isID(userID)) return ""
+
         return jwt.sign(
             {'userID': userID},
             process.env.JWT_SECRET,
@@ -20,12 +30,15 @@ class JWT_AUTH {
     }
 
     static authenticateToken(req){
-        const token = this.__extractToken(req);        
-        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
-            if (err) return reject(false);
-            if (!dataType.isDefined(payload.userID)) return reject(false)
-                
-            return resolve(true)
+        return new Promise((resolve, reject) => {
+            const token = this.__extractToken(req);     
+            jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+                console.log(err)
+                if (err) return reject(false);
+                if (!dataType.isDefined(payload.userID)) return reject(false)
+    
+                return resolve(true)
+            })
         })
     }
 
@@ -34,6 +47,7 @@ class JWT_AUTH {
             const token = this.__extractToken(req);
             jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
                 if (err) return reject(0);
+                if (!dataType.isDefined(payload.userID)) reject(0)
                 return resolve(payload.userID)
             })
         })
