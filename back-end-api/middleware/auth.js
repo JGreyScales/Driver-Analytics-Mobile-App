49b54896feb8824a74189fd1ff1108ec15a866b1
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-29 17:13:06 -0400

    basic auth written

---
+++ back-end-api/middleware/auth.js
@@ -0,0 +1,41 @@
+const jwt = require("jsonwebtoken")
+
+class JWT_AUTH {
+    constructor(){}
+
+    static __extractToken(req){
+        let token = undefined;
+        if (typeof(req) === 'string'){token = req?.split(' ')[1]}
+        else {token = req.header('Authorization')?.split(' ')[1];}
+        return token
+    }
+
+    static generateToken(userID){
+        return jwt.sign(
+            {'userID': userID},
+            process.env.JWT_SECRET,
+            {'expiresIn':'1h'}
+        )
+    }
+
+    static authenticateToken(req){
+        const token = this.__extractToken(req);        
+        jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+            if (err) return reject(false);
+            return resolve(true)
+        })
+    }
+
+    static getUserIDFromToken(req){
+        return new Promise((resolve, reject) => {
+            const token = this.__extractToken(req);
+            jwt.verify(token, process.env.JWT_SECRET, (err, payload) => {
+                if (err) return reject(0);
+                return resolve(payload.userID)
+            })
+        })
+    }
+
+}
+
+module.exports = JWT_AUTH
