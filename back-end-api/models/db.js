bc246705415aff7f466b4ceee2692b475cad9e84
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-28 23:28:58 -0400

    database Read/Write
    - added safety control functions
    - added reading
    - added writing
    
    - all tests pass

--- back-end-api/models/db.js
+++ back-end-api/models/db.js
@@ -1,10 +1,16 @@
 let mysql = require('mysql');
+const { param } = require('../routes/assurance');
 require('dotenv').config({ quiet: true });
 
 class Database {
   constructor(testing = false) {
     this.testing = testing;
     this.connection = null;
+    this.usersTable = "Users"
+
+
+
+    this.defaultTable = this.usersTable
   }
 
   async connect() {
@@ -39,6 +45,52 @@ class Database {
         resolve();
       }
     });
+  }
+
+  async dropSafety(){
+    return new Promise((resolve, reject) => {
+      this.connection.query('SET SQL_SAFE_UPDATES = 0', (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  }
+
+  async raiseSafety(){
+    return new Promise((resolve, reject) => {
+      this.connection.query('SET SQL_SAFE_UPDATES = 1', (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  }
+
+  // used for write only
+  async submitQuery(query, params, bypassNoResult = false){
+    console.log(`executing: ${mysql.format(query, params)}`)
+
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, params, (err, results) => {
+        if (err) return reject({statusCode: 400, message: `Database query error: ${err.sqlMessage}`});
+        // insertId is only 0 when truncate is ran
+        // truncate causes affectedRows to equal 0
+        if (results.affectedRows === 0 && !bypassNoResult) return reject({statusCode: 404, message: `No rows updated`});
+        resolve(results)
+      })
+    })
+  }
+
+  // used for read only
+  async fetchQuery(query, params){
+    console.log(`executing: ${mysql.format(query, params)}`)
+
+    return new Promise((resolve, reject) => {
+      this.connection.query(query, params, (err, results) => {
+        if (err) return reject({statusCode: 400, message: `Database query error: ${err.sqlMessage}`});
+        if (results.length === 0) return reject({statusCode: 404, message: `No objects found`});
+        resolve(results)
+      })
+    })
   }
 }
 
