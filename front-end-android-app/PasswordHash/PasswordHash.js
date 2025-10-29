05185097810c85f53396a9ed810825eefa981ff1
AzimAtiff-netrunner <azim.atif2006@gmail.com>
2025-10-29 16:51:55 -0400

    Commit #2
    removed the salt generation as bcrypt can generate and create a salt automatically. re-wrote the hash function and wrapped it in a class and a static async method. Used the (try.., catch..) statement for error handling. method takes a userinput(not hard-coded) and returns the userinput as a string.

--- front-end-android-app/PasswordHash/PasswordHash.js
+++ front-end-android-app/PasswordHash/PasswordHash.js
@@ -1,20 +1,19 @@
 const bcrypt = require('bcrypt');//include bcrypt module
-const saltRounds = 10; //dictates he computational cost of hashing and, consequently, the level of security
 
-bcrypt.genSalt(saltRounds, (err, salt) => {
-if (err) {
-    errormsg.Text = "Unable to generate salt"; 
-    return;
+
+class PasswordHash {
+    static saltRounds = 12; //dictates he computational cost of hashing and, consequently, the level of security 
+
+    static async HashMethod(userPassword) {//takes input 
+        try { //code that will cath error
+            const hash = await bcrypt.hash(userPassword, this.saltRounds); 
+            console.log("Hashed Password: ", hash); 
+            return hash; 
+
+        }catch(err) { //what to do with the error
+            console.error("Unable to Hash Password: ", err);  
+            throw err; 
+        }
+        console.log("Hashed Password: ", hash); //hashing successful
+        }
 }
-console.log("Salt Generation Successful"); // Salt generation successful, proceed to hash the password
-});
-
-const userPassword = '1234password'; 
-bcrypt.hash(userPassword, salt, (err, hash) => {
-    if(err){
-        errormsg.Text = "unable to hash user password";
-    }
-
-    console.log("Hashed Password: ", hash); //hashing successful
-});
-
