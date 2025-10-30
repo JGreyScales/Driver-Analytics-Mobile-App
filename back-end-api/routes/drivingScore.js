116ddb19cb55e10c2223c18c34d64133982e873c
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-30 04:54:34 -0400

    added apiEndpoint
    I dont know why I thought we didnt need an endpoint, it has now been added
    
    ------------------|---------|----------|---------|---------|-------------------
    File              | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ------------------|---------|----------|---------|---------|-------------------
    All files         |   96.39 |     82.9 |   94.23 |   98.14 |
     controllers      |   93.85 |    81.25 |     100 |   95.14 |
      drivingScore.js |   97.36 |    86.66 |     100 |   97.05 | 76
      user.js         |    92.1 |    78.78 |     100 |    94.2 | 48,70,93,123
     middleware       |   93.33 |    83.33 |   88.88 |     100 |
      auth.js         |   93.33 |    83.33 |   88.88 |     100 | 37,48
     models           |   95.83 |    91.66 |     100 |     100 |
      db.js           |   95.83 |    91.66 |     100 |     100 | 51-60
     routes           |     100 |    77.41 |   88.88 |     100 |
      assurance.js    |     100 |      100 |     100 |     100 |
      authCheck.js    |     100 |      100 |     100 |     100 |
      drivingScore.js |     100 |      100 |      50 |     100 |
      user.js         |     100 |       65 |     100 |     100 | 20-65
     utils            |     100 |      100 |   83.33 |     100 |
      dataType.js     |     100 |      100 |   83.33 |     100 |
    ------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 9 passed, 9 total
    Tests:       119 passed, 119 total
    Snapshots:   0 total
    Time:        107.191 s
    Ran all test suites.

--- back-end-api/routes/drivingScore.js
+++ back-end-api/routes/drivingScore.js
@@ -1,5 +1,41 @@
 const express = require("express");
 const router = express.Router();
+const Driving_Score = require('../controllers/drivingScore')
+const dataTypes = require("../utils/dataType")
+const JWT_AUTH = require("../middleware/auth")
+
+router.put("/score", async(req, res) => {
+  // required body [tripDuration, incidentCount, averageSpeed, maxSpeed]
+    let testing = false
+    if (req.body) {testing = dataTypes.isDefined(req.body.testing)}
+    else {res.status(400).send({statusCode:400, message:'No body attached'})}
+    if(testing) {console.log("Running delete user in test mode")}
+    
+    let userID = undefined
+    try {
+        userID = await JWT_AUTH.getUserIDFromToken(req)
+    } catch (err) {
+        res.status(400).send({statusCode:400, message:'No token attached'})
+    }
+
+    let tripDuration = req.body.tripDuration
+    let incidentCount = req.body.incidentCount
+    let averageSpeed = req.body.averageSpeed
+    let maxSpeed = req.body.maxSpeed
+
+    if (!dataTypes.isValidDrivingParam(tripDuration) ||
+    !Number.isInteger(incidentCount) ||
+     !dataTypes.isValidDrivingParam(averageSpeed) ||
+      !dataTypes.isValidDrivingParam(maxSpeed) ||
+        maxSpeed < averageSpeed){
+        res.status(400).send({statusCode:400, message:"Invalid body"})
+      }
+
+    const DS = new Driving_Score(testing)
+    const result = await DS.uploadNewDrivingScore(tripDuration, incidentCount, averageSpeed, maxSpeed, userID)
+
+    res.status(result.statusCode).send(result)
+})
 
 router.get("/comparativeScore", async (req, res) => {
 
