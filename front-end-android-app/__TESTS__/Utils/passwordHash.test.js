486b6e21f97fa0910e4e0ebe56c8cec01d05a18e
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-10-29 21:17:57 -0400

    re-organized-test-cases
     PASS  __TESTS__/Utils/passwordHash.test.js
      Hash Method
        √ should hash password (204 ms)
        √ should deny ints (1 ms)
        √ should deny floats
        √ should deny undefined (1 ms)
        √ should deny empty (1 ms)
    
    -----------------|---------|----------|---------|---------|-------------------
    File             | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    -----------------|---------|----------|---------|---------|-------------------
    All files        |     100 |      100 |     100 |     100 |
     passwordHash.js |     100 |      100 |     100 |     100 |
    -----------------|---------|----------|---------|---------|-------------------
    Test Suites: 1 passed, 1 total
    Tests:       5 passed, 5 total
    Snapshots:   0 total
    Time:        0.775 s, estimated 1 s
    Ran all test suites.

--- front-end-android-app/__TESTS__/Utils/passwordHash.test.js
+++ front-end-android-app/__TESTS__/Utils/passwordHash.test.js
@@ -1,36 +1,35 @@
 const bcrypt = require('bcrypt'); 
-const PasswordHash = require("../../front-end-android-app/utils/PasswordHash/PasswordHash.js"); 
+const PasswordHash = require("../../Utils/passwordHash")
 
 describe('Hash Method', () => {
     test('should hash password', async () => {
         const passwordInput = "pass122345"; 
         const hashed = await PasswordHash.HashMethod(passwordInput);
-        const compare = await bcrypt.compare(passwordInput, hashed); 
-        expect(compare).toBe(true); 
+        await expect(bcrypt.compare(passwordInput, hashed)).toBeTruthy()
     })
 
 
     test('should deny ints', async () => {
         const passwordInput = -123455; 
         const hashed = await PasswordHash.HashMethod(passwordInput); 
-        await expect(hashed).toBeFalsy(); 
+        await expect(hashed).toBe(""); 
     })
 
     test('should deny floats', async () => {
         const passwordInput = 1.0022; 
         const hashed = await PasswordHash.HashMethod(passwordInput); 
-        await expect(hashed).toBeFalsy();  
+        await expect(hashed).toBe("");  
     })
 
     test('should deny undefined', async () => {
         const passwordInput = undefined; 
         const hashed = await PasswordHash.HashMethod(passwordInput); 
-        await expect(hashed).toBeFalsy();  
+        await expect(hashed).toBe("");  
     })
 
     test('should deny empty', async () => {
         const passwordInput = ""; 
         const hashed = await PasswordHash.HashMethod(passwordInput); 
-        await expect(hashed).toBeFalsy();  
+        await expect(hashed).toBe("");  
     })
 });
