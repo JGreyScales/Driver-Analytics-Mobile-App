fb240c375fc709bb9ad67ce7b4bb1dc1fc166175
AzimAtiff-netrunner <azim.atif2006@gmail.com>
2025-10-29 20:50:29 -0400

    Commit#4
    added a check condition in PasswordHash.js. Added more test cases in PasswordHash.test.js.

--- back-end-api/PasswordHashTests/PasswordHash.test.js
+++ back-end-api/PasswordHashTests/PasswordHash.test.js
@@ -1,11 +1,36 @@
 const bcrypt = require('bcrypt'); 
-const PasswordHash = require("../PasswordHash/PasswordHash.js"); 
+const PasswordHash = require("../../front-end-android-app/utils/PasswordHash/PasswordHash.js"); 
 
 describe('Hash Method', () => {
-    test('should hash password correctly', async () => {
+    test('should hash password', async () => {
         const passwordInput = "pass122345"; 
         const hashed = await PasswordHash.HashMethod(passwordInput);
         const compare = await bcrypt.compare(passwordInput, hashed); 
         expect(compare).toBe(true); 
+    })
+
+
+    test('should deny ints', async () => {
+        const passwordInput = -123455; 
+        const hashed = await PasswordHash.HashMethod(passwordInput); 
+        await expect(hashed).toBeFalsy(); 
+    })
+
+    test('should deny floats', async () => {
+        const passwordInput = 1.0022; 
+        const hashed = await PasswordHash.HashMethod(passwordInput); 
+        await expect(hashed).toBeFalsy();  
+    })
+
+    test('should deny undefined', async () => {
+        const passwordInput = undefined; 
+        const hashed = await PasswordHash.HashMethod(passwordInput); 
+        await expect(hashed).toBeFalsy();  
+    })
+
+    test('should deny empty', async () => {
+        const passwordInput = ""; 
+        const hashed = await PasswordHash.HashMethod(passwordInput); 
+        await expect(hashed).toBeFalsy();  
     })
 });
