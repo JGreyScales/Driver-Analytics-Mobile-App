cb0a6ca1898f6ac18abd1f319b4897973299953e
GreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-11-05 13:52:04 -0500

    Update SignUpScreen.test.js

--- front-end-android-app/__TESTS__/src/SignUpScreen.test.js
+++ front-end-android-app/__TESTS__/src/SignUpScreen.test.js
@@ -1,11 +1,11 @@
-// import React from "react";
-// import renderer, { act } from "react-test-renderer";
-// import SignUpScreen from "../../src/screens/SignUpScreen";
+import React from "react";
+import renderer, { act } from "react-test-renderer";
+import SignUpScreen from "../../src/screens/SignUpScreen";
 
-// test("SignUpScreen renders correctly", async () => {
-//   let tree;
-//   await act(async () => {
-//     tree = renderer.create(<SignUpScreen />);
-//   });
-//   expect(tree.toJSON()).toMatchSnapshot();
-// });
+test("SignUpScreen renders correctly", async () => {
+  let tree;
+  await act(async () => {
+    tree = renderer.create(<SignUpScreen />);
+  });
+  expect(tree.toJSON()).toMatchSnapshot();
+});
