08f301bd91fd6bbf35988365e2b343773d166f0c
GreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-11-06 11:10:57 -0500

    post user tested & test (#42)
    --------------------|---------|----------|---------|---------|-------------------
    File                | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    --------------------|---------|----------|---------|---------|-------------------
    All files           |   71.02 |    57.35 |      75 |      75 |
     screens            |   62.65 |    54.68 |   63.63 |    67.1 |
      SignInScreen.js   |   97.56 |      100 |   83.33 |     100 |
      SignUpScreen.js   |   28.57 |    19.44 |      40 |   32.43 | 34-46,50-78,83
     styles             |     100 |      100 |     100 |     100 |
      GlobalStyles.js   |     100 |      100 |     100 |     100 |
     utils              |     100 |      100 |     100 |     100 |
      SessionManager.js |     100 |      100 |     100 |     100 |
      passwordHash.js   |     100 |      100 |     100 |     100 |
    --------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 4 passed, 4 total
    Tests:       25 passed, 25 total
    Snapshots:   1 passed, 1 total
    Time:        2.793 s, estimated 6 s
    Ran all test suites.
    
    ------------------|---------|----------|---------|---------|-------------------
    File              | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ------------------|---------|----------|---------|---------|-------------------
    All files         |   96.25 |    82.11 |   96.77 |    97.5 |
     controllers      |   93.47 |    81.96 |     100 |    94.4 |
      drivingScore.js |   95.16 |    85.71 |     100 |   94.64 | 76,109,116
      user.js         |    92.1 |    78.78 |     100 |    94.2 | 48,70,93,123
     middleware       |   93.75 |    83.33 |   88.88 |     100 |
      auth.js         |   93.75 |    83.33 |   88.88 |     100 | 44,55
     models           |   94.11 |    86.66 |     100 |   97.72 |
      db.js           |   94.11 |    86.66 |     100 |   97.72 | 17
     routes           |     100 |    74.28 |     100 |     100 |
      assurance.js    |     100 |      100 |     100 |     100 |
      authCheck.js    |     100 |      100 |     100 |     100 |
      drivingScore.js |     100 |    86.66 |     100 |     100 | 42-43
      user.js         |     100 |       65 |     100 |     100 | 20-65
     utils            |     100 |      100 |   83.33 |     100 |
      dataType.js     |     100 |      100 |   83.33 |     100 |
     validation       |   98.14 |     90.9 |     100 |   98.11 |
      drivingScore.js |     100 |      100 |     100 |     100 |
      generic.js      |   95.23 |     90.9 |     100 |      95 | 9
      user.js         |     100 |      100 |     100 |     100 |
    ------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 12 passed, 12 total
    Tests:       155 passed, 155 total
    Snapshots:   0 total
    Time:        131.476 s
    Ran all test suites.

--- back-end-api/__TESTS__/validation/user.test.js
+++ back-end-api/__TESTS__/validation/user.test.js
@@ -1,102 +1,100 @@
-const {validateGetUser, validatePutUser} = require('../../validation/user')
+const { validateGetUser, validatePutUser, validatePostUser } = require('../../validation/user')
 const JWT_AUTH = require("../../middleware/auth")
 
 describe('getUser', () => {
-    it('should call next() when the token and request are valid', async () => {
-        const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
-      
-        const req = {
-          header: {
-            Authorization: JWT
-          },
-          body: {} // no invalid fields
-        };
-      
-        const res = {
-          status: jest.fn().mockReturnThis(),
-          send: jest.fn()
-        };
-      
-        const next = jest.fn();
-      
-        await validateGetUser(req, res, next);
-      
-        expect(next).toHaveBeenCalled();
-        expect(res.status).not.toHaveBeenCalled();
-        expect(res.send).not.toHaveBeenCalled();
-      });
-      
+  it('should call next() when the token and request are valid', async () => {
+    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
 
-    it('should reject an invalid token', async () => {
-        const JWT = `Bearer ${JWT_AUTH.generateToken(1)}3`;
-      
-        const req = {
-          header: {
-            Authorization: JWT
-          }
-        };
-      
-        const res = {
-          status: jest.fn().mockReturnThis(),
-          send: jest.fn()
-        };
-      
-        await validateGetUser(req, res);
-      
-        // Assert the correct response
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.send).toHaveBeenCalledWith('No token attached');
-      });
-      it('should reject if an auth header is not present', async () => {    
-        const req = {
-          header: {
-          }
-        };
-      
-        const res = {
-          status: jest.fn().mockReturnThis(),
-          send: jest.fn()
-        };
-      
-        await validateGetUser(req, res);
-      
-        // Assert the correct response
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.send).toHaveBeenCalledWith('No token attached');
-    })
+    const req = {
+      header: {
+        Authorization: JWT
+      },
+      body: {} // no invalid fields
+    };
 
-    it('should reject if body is present', async () => {
-        const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
-      
-        const req = {
-          header: {
-            Authorization: JWT
-          },
-          body: {
-            xx: 'xx'
-          }
-        };
-      
-        const res = {
-          status: jest.fn().mockReturnThis(),
-          send: jest.fn()
-        };
-      
-        await validateGetUser(req, res);
-      
-        // Assert the correct response
-        expect(res.status).toHaveBeenCalledWith(400);
-        expect(res.send).toHaveBeenCalledWith('xx is not a valid field for this request');
-    })
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    const next = jest.fn();
+
+    await validateGetUser(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.send).not.toHaveBeenCalled();
+  });
+
+
+  it('should reject an invalid token', async () => {
+    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}3`;
+
+    const req = {
+      header: {
+        Authorization: JWT
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    await validateGetUser(req, res);
+
+    // Assert the correct response
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith('No token attached');
+  });
+  it('should reject if an auth header is not present', async () => {
+    const req = {
+      header: {
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    await validateGetUser(req, res);
+
+    // Assert the correct response
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith('No token attached');
+  })
+
+  it('should reject if body is present', async () => {
+    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
+
+    const req = {
+      header: {
+        Authorization: JWT
+      },
+      body: {
+        xx: 'xx'
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    await validateGetUser(req, res);
+
+    // Assert the correct response
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith('xx is not a valid field for this request');
+  })
 })
 
 describe('putUser', () => {
   it('should allow a valid user entry to pass', async () => {
-    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
-      
+
     const req = {
       header: {
-        Authorization: JWT
       },
       body: {
         username: 'xx',
@@ -104,93 +102,160 @@ describe('putUser', () => {
         passwordHash: 'yyy'
       }
     };
-  
+
     const res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn()
     };
-  
+
     const next = jest.fn();
-  
+
     await validatePutUser(req, res, next);
-  
+
     expect(next).toHaveBeenCalled();
     expect(res.status).not.toHaveBeenCalled();
     expect(res.send).not.toHaveBeenCalled();
   })
 
-  it('should deny if token is invalid', async () => {
-    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}2`;
-      
-    const req = {
-      header: {
-        Authorization: JWT
-      },
-      body: {
-        username: 'xx',
-        email: 'ttt',
-        passwordHash: 'yyy'
-      }
-    };
-  
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      send: jest.fn()
-    };
-  
-    const next = jest.fn();
-  
-    await validatePutUser(req, res, next);
-  
-    expect(res.status).toHaveBeenCalledWith(401);
-    expect(res.send).toHaveBeenCalledWith('No token attached');
-  })
 
   it('should deny if not all fields are present', async () => {
-    const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
-      
     const req = {
       header: {
-        Authorization: JWT
       },
       body: {
         username: 'xx',
       }
     };
-  
+
     const res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn()
     };
-  
+
     const next = jest.fn();
-  
+
     await validatePutUser(req, res, next);
-  
+
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.send).toHaveBeenCalledWith('Missing required fields: email, passwordHash');
   })
 
   it('should deny if body is missing', async () => {
     const JWT = `Bearer ${JWT_AUTH.generateToken(1)}`;
-      
+
     const req = {
       header: {
         Authorization: JWT
       },
 
     };
-  
+
     const res = {
       status: jest.fn().mockReturnThis(),
       send: jest.fn()
     };
-  
+
     const next = jest.fn();
-  
+
     await validatePutUser(req, res, next);
-  
+
     expect(res.status).toHaveBeenCalledWith(400);
     expect(res.send).toHaveBeenCalledWith('Missing required fields: username, email, passwordHash');
+  })
+})
+
+describe('postUser', () => {
+  it('should allow a valid request through', async () => {
+    JWT = `Bearer ${JWT_AUTH.generateToken(1)}`
+    const req = {
+      header: {
+        Authorization: JWT
+      },
+      body: {
+        username: 'xx',
+        passwordHash: 'xxxx'
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    const next = jest.fn();
+
+    await validatePostUser(req, res, next);
+
+    expect(next).toHaveBeenCalled();
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.send).not.toHaveBeenCalled();
+  })
+
+  it('should deny if no token', async () => {
+
+    const req = {
+      header: {
+      },
+      body: {
+        username: 'xx',
+        passwordHash: 'tyyy'
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    await validateGetUser(req, res);
+
+    // Assert the correct response
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.send).toHaveBeenCalledWith('No token attached');
+  })
+
+  it('it should deny if body is not valid', async () => {
+    JWT = `Bearer ${JWT_AUTH.generateToken(1)}`
+    const req = {
+      header: {
+        Authorization: JWT
+      },
+      body: {
+        username: 'xx',
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    const next = jest.fn();
+
+    await validatePostUser(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith('Missing required fields: passwordHash');
+  })
+
+  it('should deny if unrequested body is present', async () => {
+    JWT = `Bearer ${JWT_AUTH.generateToken(1)}`
+    const req = {
+      header: {
+        Authorization: JWT
+      },
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      send: jest.fn()
+    };
+
+    const next = jest.fn();
+
+    await validatePostUser(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.send).toHaveBeenCalledWith('Missing required fields: username, passwordHash');
   })
 })
