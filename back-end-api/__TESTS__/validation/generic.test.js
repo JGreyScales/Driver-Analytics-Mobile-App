5d109258bf2415909faeceb544611b05d431e8db
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-11-05 21:50:17 -0500

    all tests passing
    --------------------|---------|----------|---------|---------|-------------------
    File                | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    --------------------|---------|----------|---------|---------|-------------------
    All files           |   71.02 |    57.35 |      75 |      75 |
     screens            |   62.65 |    54.68 |   63.63 |    67.1 |
      SignInScreen.js   |   97.56 |      100 |   83.33 |     100 |
      SignUpScreen.js   |   28.57 |    19.44 |      40 |   32.43 | 34-46,50-78,83
     styles             |     100 |      100 |     100 |     100 |
      GlobalStyles.js   |     100 |      100 |     100 |     100 |
     utils              |     100 |      100 |     100 |     100 |
      SessionManager.js |     100 |      100 |     100 |     100 |
      passwordHash.js   |     100 |      100 |     100 |     100 |
    --------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 4 passed, 4 total
    Tests:       25 passed, 25 total
    Snapshots:   1 passed, 1 total
    Time:        2.441 s
    Ran all test suites.
    
    ------------------|---------|----------|---------|---------|-------------------
    File              | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ------------------|---------|----------|---------|---------|-------------------
    All files         |    96.2 |    82.11 |   96.72 |   97.46 |
     controllers      |   93.47 |    81.96 |     100 |    94.4 |
      drivingScore.js |   95.16 |    85.71 |     100 |   94.64 | 76,109,116
      user.js         |    92.1 |    78.78 |     100 |    94.2 | 48,70,93,123
     middleware       |   93.75 |    83.33 |   88.88 |     100 |
      auth.js         |   93.75 |    83.33 |   88.88 |     100 | 44,55
     models           |   94.11 |    86.66 |     100 |   97.72 |
      db.js           |   94.11 |    86.66 |     100 |   97.72 | 17
     routes           |     100 |    74.28 |     100 |     100 |
      assurance.js    |     100 |      100 |     100 |     100 |
      authCheck.js    |     100 |      100 |     100 |     100 |
      drivingScore.js |     100 |    86.66 |     100 |     100 | 42-43
      user.js         |     100 |       65 |     100 |     100 | 20-65
     utils            |     100 |      100 |   83.33 |     100 |
      dataType.js     |     100 |      100 |   83.33 |     100 |
     validation       |   97.95 |     90.9 |     100 |   97.91 |
      drivingScore.js |     100 |      100 |     100 |     100 |
      generic.js      |   95.23 |     90.9 |     100 |      95 | 9
      user.js         |     100 |      100 |     100 |     100 |
    ------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 12 passed, 12 total
    Tests:       152 passed, 152 total
    Snapshots:   0 total
    Time:        132.447 s, estimated 137 s
    Ran all test suites.

--- back-end-api/__TESTS__/validation/generic.test.js
+++ back-end-api/__TESTS__/validation/generic.test.js
@@ -69,4 +69,17 @@ describe('validate fields', () => {
 
         await expect(validateFields(req, ALLOWED_VALUES, true)).rejects.toStrictEqual({statusCode: 400, message:"Missing required fields: username"})
     })
+
+    it('should allow testing to pass through', async () => {
+        ALLOWED_VALUES = ["userID", "password", "username", "tye"]
+        req = {}
+        req.body = {}
+        req.body.userID = 4234
+        req.body.password = "pass"
+        req.body.username = "user"
+        req.body.tye = 3242
+        req.body.testing = true
+
+        await expect(validateFields(req, ALLOWED_VALUES)).resolves.toBeTruthy()
+    })
 })
