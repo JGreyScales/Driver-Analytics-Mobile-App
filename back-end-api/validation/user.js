4a77f1a6cff08416209716cce51934f7eff84c66
JGreyScales <77678089+JGreyScales@users.noreply.github.com>
2025-11-06 11:09:12 -0500

    post user tested & test
    --------------------|---------|----------|---------|---------|-------------------
    File                | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    --------------------|---------|----------|---------|---------|-------------------
    All files           |   71.02 |    57.35 |      75 |      75 |
     screens            |   62.65 |    54.68 |   63.63 |    67.1 |
      SignInScreen.js   |   97.56 |      100 |   83.33 |     100 |
      SignUpScreen.js   |   28.57 |    19.44 |      40 |   32.43 | 34-46,50-78,83
     styles             |     100 |      100 |     100 |     100 |
      GlobalStyles.js   |     100 |      100 |     100 |     100 |
     utils              |     100 |      100 |     100 |     100 |
      SessionManager.js |     100 |      100 |     100 |     100 |
      passwordHash.js   |     100 |      100 |     100 |     100 |
    --------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 4 passed, 4 total
    Tests:       25 passed, 25 total
    Snapshots:   1 passed, 1 total
    Time:        2.793 s, estimated 6 s
    Ran all test suites.
    
    ------------------|---------|----------|---------|---------|-------------------
    File              | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ------------------|---------|----------|---------|---------|-------------------
    All files         |   96.25 |    82.11 |   96.77 |    97.5 |
     controllers      |   93.47 |    81.96 |     100 |    94.4 |
      drivingScore.js |   95.16 |    85.71 |     100 |   94.64 | 76,109,116
      user.js         |    92.1 |    78.78 |     100 |    94.2 | 48,70,93,123
     middleware       |   93.75 |    83.33 |   88.88 |     100 |
      auth.js         |   93.75 |    83.33 |   88.88 |     100 | 44,55
     models           |   94.11 |    86.66 |     100 |   97.72 |
      db.js           |   94.11 |    86.66 |     100 |   97.72 | 17
     routes           |     100 |    74.28 |     100 |     100 |
      assurance.js    |     100 |      100 |     100 |     100 |
      authCheck.js    |     100 |      100 |     100 |     100 |
      drivingScore.js |     100 |    86.66 |     100 |     100 | 42-43
      user.js         |     100 |       65 |     100 |     100 | 20-65
     utils            |     100 |      100 |   83.33 |     100 |
      dataType.js     |     100 |      100 |   83.33 |     100 |
     validation       |   98.14 |     90.9 |     100 |   98.11 |
      drivingScore.js |     100 |      100 |     100 |     100 |
      generic.js      |   95.23 |     90.9 |     100 |      95 | 9
      user.js         |     100 |      100 |     100 |     100 |
    ------------------|---------|----------|---------|---------|-------------------
    
    Test Suites: 12 passed, 12 total
    Tests:       155 passed, 155 total
    Snapshots:   0 total
    Time:        131.476 s
    Ran all test suites.

--- back-end-api/validation/user.js
+++ back-end-api/validation/user.js
@@ -15,8 +15,6 @@ async function validateGetUser(req, res, next) {
 
 async function validatePutUser(req, res, next) {
     try {
-        await genericValidation(req);
-
         const ALLOWED_FIELDS = ["username", "email", "passwordHash"] ;
         await validateFields(req, ALLOWED_FIELDS, true);
 
@@ -26,4 +24,17 @@ async function validatePutUser(req, res, next) {
     }
 }
 
-module.exports = { validateGetUser, validatePutUser };
+async function validatePostUser(req, res, next) {
+    try {
+        await genericValidation(req)
+        
+        const ALLOWED_FIELDS = ['username', 'passwordHash']
+        await validateFields(req, ALLOWED_FIELDS, true)
+
+        next()
+    } catch (err) {
+        res.status(err.statusCode).send(err.message)
+    }
+}
+
+module.exports = { validateGetUser, validatePutUser, validatePostUser };
